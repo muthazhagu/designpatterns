b5247e2a3da1b68eb554ad46d7dc1afb00a557a2
Muthu Palanisamy <muthazhagu@gmail.com>
2016-08-12 17:46:50 -0400

    Building the iterator.

--- src/main/java/iterator/Iterator.java
+++ src/main/java/iterator/Iterator.java
@@ -4,4 +4,51 @@ package main.java.iterator;
  * Created by mpalanisamy on 8/11/16.
  */
 public class Iterator {
+    private int currentIndex = -1;
+    private int length = 0;
+    private Product[] p;
+
+    public Iterator(Product[] p){
+        this.p = p;
+        length = p.length;
+    }
+
+    public Product getNext(){
+        if (length == 0){
+            return null;
+        }
+
+        currentIndex += 1;
+        if (currentIndex < length){
+            return p[currentIndex];
+        } else{
+            return null;
+        }
+    }
+
+    public static void main(String[] args){
+        Iterator myIterator = new Iterator(new Product[] {});
+
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+        myIterator = new Iterator(new Product[] {new Product()});
+
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+        myIterator = new Iterator(new Product[] {new Product()});
+
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+        myIterator = new Iterator(new Product[] {new Product(), new Product()});
+
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+
+        System.out.println();
+    }
 }
--- src/main/java/iterator/ProductPortfolio.java
+++ src/main/java/iterator/ProductPortfolio.java
@@ -4,4 +4,34 @@ package main.java.iterator;
  * Created by mpalanisamy on 8/11/16.
  */
 public class ProductPortfolio {
+    private Product[] products;
+
+    public ProductPortfolio(Product[] p){
+        products = p;
+    }
+
+    public Iterator createIterator(){
+        return new Iterator(products);
+    }
+
+    public static void main(String[] args){
+        ProductPortfolio myProductPortfolio = new ProductPortfolio(new Product[]{});
+        Iterator myIterator = myProductPortfolio.createIterator();
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+        myProductPortfolio = new ProductPortfolio(new Product[]{new Product()});
+        myIterator = myProductPortfolio.createIterator();
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+        myProductPortfolio = new ProductPortfolio(new Product[]{new Product(), new Product()});
+        myIterator = myProductPortfolio.createIterator();
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+        System.out.println(myIterator.getNext());
+        System.out.println();
+
+    }
 }
