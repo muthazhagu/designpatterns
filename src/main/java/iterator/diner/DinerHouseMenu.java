de8b2ca1bfb5bb309f66f12d2776ff299be1d940
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2016-08-13 20:10:47 -0400

    Learning iterators with a second example.

---
+++ src/main/java/iterator/diner/DinerHouseMenu.java
@@ -0,0 +1,8 @@
+package main.java.iterator.diner;
+
+/**
+ * Created by muthu on 8/13/16.
+ */
+public class DinerHouseMenu {
+
+}
---
+++ src/main/java/iterator/diner/PancakeHouseMenu.java
@@ -0,0 +1,43 @@
+package main.java.iterator.diner;
+
+import java.util.ArrayList;
+
+/**
+ * Created by muthu on 8/13/16.
+ */
+public class PancakeHouseMenu {
+    private ArrayList<MenuItem> menuItems;
+
+    public PancakeHouseMenu(){
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("Pancake BrkFast",
+                "Pancakes with egg and bacon",
+                false,
+                2.99);
+
+        addItem("Pancake Regular",
+                "Pancakes with egg and bacon, and toast",
+                false,
+                2.99);
+
+        addItem("Waffles BrkFast",
+                "Waffles with egg and bacon",
+                false,
+                3.49);
+
+        addItem("Waffles Regular",
+                "Waffles with egg and bacon, and toast",
+                false,
+                3.49);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+}
