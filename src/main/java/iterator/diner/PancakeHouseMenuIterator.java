3df86a64829a8d4eedd8f7da1430b3ea32b57c49
Muthu Palanisamy <muthazhagu@gmail.com>
2016-08-15 17:22:49 -0400

    Added iterator for pancake house menu.

---
+++ src/main/java/iterator/diner/PancakeHouseMenuIterator.java
@@ -0,0 +1,33 @@
+package main.java.iterator.diner;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mpalanisamy on 8/15/16.
+ */
+public class PancakeHouseMenuIterator implements Iterator{
+    private ArrayList<MenuItem> menuItems;
+    private int currenPosition = -1;
+
+    public PancakeHouseMenuIterator(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public boolean hasNext() {
+        return (menuItems.size() != 0) && (currenPosition+1 < menuItems.size());
+    }
+
+    public Object next() {
+        if (menuItems.size() == 0) {
+            return null;
+        }
+
+        currenPosition += 1;
+
+        if (currenPosition >= menuItems.size()) {
+            return null;
+        } else {
+            return menuItems.get(currenPosition);
+        }
+    }
+}
