cd12950c0e042ef97600e06d34dbd0e8215d6eae
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2016-08-23 22:50:08 -0400

    Adding the Menu class.

---
+++ src/main/java/composite/diner/Menu.java
@@ -0,0 +1,49 @@
+package main.java.composite.diner;
+
+import java.util.ArrayList;
+
+/**
+ * Created by muthu on 8/23/16.
+ */
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+    String name;
+    String description;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    @Override
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    @Override
+    public MenuComponent getChild(int index) {
+        return menuComponents.get(index);
+    }
+
+    @Override
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("_____________________");
+    }
+}
