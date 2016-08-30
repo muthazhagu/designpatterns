abe5a29a2a0675f6184860f1b825eb9135658024
Muthu Palanisamy <muthazhagu@gmail.com>
2016-08-30 16:22:39 -0400

    Adding the NullIterator.

--- src/main/java/composite/diner/NullIterator.java
+++ src/main/java/composite/diner/NullIterator.java
@@ -1,7 +1,24 @@
 package main.java.composite.diner;
 
+import java.util.Iterator;
+
 /**
  * Created by mpalanisamy on 8/30/16.
  */
-public class NullIterator {
+public class NullIterator implements Iterator {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
 }
